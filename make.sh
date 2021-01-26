79afe899b54c6692fb16e34b913f1e370bd86f85
miqt <miqtdev@163.com>
2021-01-26 13:15:32 +0800

    增加攻速图片识别支持

--- make.sh
+++ make.sh
@@ -10,6 +10,7 @@ pyinstaller -F --uac-admin -w 丝滑走A_寒冰自动Q.py -i icon.ico
 pyinstaller -F --uac-admin -w 丝滑走A_大嘴自动W.py -i icon.ico
 pyinstaller -F --uac-admin -w 丝滑走A_天使自动E.py -i icon.ico
 pyinstaller -F --uac-admin -w 丝滑走A_老鼠自动R.py -i icon.ico
+pyinstaller -F --uac-admin -w 丝滑走A_通用_自动识别攻速.py -i icon.ico
 rm -rf __pycache__
 rm -rf build
 rm -rf *.spec
